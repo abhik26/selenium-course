30d281842aeabf4745a2d306d32258a58ade3a9e
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2022-06-27 21:11:07 +0530

    Add assignments, calendar selector, end to end program and browser alert support.

---
+++ src/main/java/com/example/selenium_learning/BrowserAlert.java
@@ -0,0 +1,33 @@
+package com.example.selenium_learning;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserAlert {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:/Softwares/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice");
+		driver.findElement(By.id("name")).sendKeys("Abhishek");
+		driver.findElement(By.id("alertbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		Thread.sleep(1000);
+		driver.switchTo().alert().accept();
+		
+		driver.findElement(By.id("name")).sendKeys("Abhishek");
+		driver.findElement(By.id("confirmbtn")).click();
+		Alert confirm = driver.switchTo().alert();
+		System.out.println(confirm.getText());
+		Thread.sleep(1000);
+		confirm.dismiss();
+		
+		Thread.sleep(2000);
+		driver.quit();
+        
+	}
+
+}
---
+++ src/main/java/com/example/selenium_learning/assignment/Assignment1.java
@@ -0,0 +1,28 @@
+package com.example.selenium_learning.assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assignment1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:/Softwares/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		Thread.sleep(1000);
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+}
